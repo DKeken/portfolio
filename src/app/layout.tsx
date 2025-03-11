2c1519ad78d9d9fa9130bce4a3779fa1cdefc668
Daniil Shovkunov <dshovkunov@intas-company.com>
2025-03-11 05:03:19 +0500

    feat(metrika): add yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import "./globals.css";
 import { ThemeSwitcher } from "@/components/ui/theme-switcher";
 import { LocaleSwitcher } from "@/components/ui/locale-switcher";
+import { YandexMetrika } from "@/components/_yandex-metrika";
+import { Suspense } from "react";
+
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -43,6 +46,28 @@ export default async function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=5.0, viewport-fit=cover"
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: ` (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(100291191, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });`,
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<div><img src="https://mc.yandex.ru/watch/100291191" style="position:absolute; left:-9999px;" alt="" /></div>`,
+          }}
+        />
       </head>
       <body className={`${montserrat.variable} h-full w-full overflow-x-auto`}>
         <I18nProvider>
@@ -56,6 +81,9 @@ export default async function RootLayout({
               <LocaleSwitcher />
               <ThemeSwitcher />
             </div>
+            <Suspense>
+              <YandexMetrika />
+            </Suspense>
             <ScrollArea className="h-full w-full relative">
               {children}
             </ScrollArea>
